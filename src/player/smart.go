c988f43edc85e689517fd35b9c4f66de70d2b57b
Matias Grioni <matgrioni@gmail.com>
2017-06-08 19:15:32 -0700

    Implemented a smart, albeit rule based discard function.

--- src/player/smart.go
+++ src/player/smart.go
@@ -33,21 +33,70 @@ func (p *SmartPlayer) Pickup(hand [5]deck.Card, top deck.Card, who int) bool {
     return r.Intn(2) == 1
 }
 
+
+
+
+
+// TODO: Change contract to just give back number.
 func (p *SmartPlayer) Discard(hand [5]deck.Card,
                               top deck.Card) ([5]deck.Card, deck.Card) {
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+    // First construct a map that holds counts for all of the suits.
+    suitsCount := make(map[deck.Suit]int)
+    lowest := make(map[deck.Suit]int)
+    for i , card := range hand {
+        adjSuit := hand[i].AdjSuit(top.Suit)
+        suitsCount[adjSuit]++
+        if _, ok := lowest[adjSuit]; !ok {
+            lowest[adjSuit] = i
+        }
 
-    total := hand[:]
-    total = append(total, top)
+        if int(card.Value) < int(hand[lowest[adjSuit]].Value) {
+            lowest[adjSuit] = i
+        }
+    }
 
-    i := r.Intn(len(total))
-    chosen := total[i]
-    total[i] = total[len(total) - 1]
-    total = total[:len(total) - 1]
+    minCard := deck.Card { top.Suit, deck.J }
+    minIndex := -1
+    singleFound := false
+    for suit, i := range lowest {
+        card := hand[i]
 
-    copy(hand[:], total[:5])
+        // If there's only one of a card that is not trump and is not an A and
+        // the current min card is of greater value (it is trump or its value is
+        // less), then update the trackers.
+        if suitsCount[suit] == 1 && suit != top.Suit && card.Value != deck.A &&
+           (minCard.AdjSuit(top.Suit) == top.Suit || int(card.Value) < int(minCard.Value)) {
+            singleFound = true
+            minCard = card
+            minIndex = i
+        } else if !singleFound {
+        // If a single card that is non-trump and non-ace has not been found
+        // then try to find the smallest card otherwise. In other words, any
+        // single suit card is preferred to a multi-suit card as long as said
+        // card is not trump or A.
 
-    return hand, chosen
+            // If the two cards of the same suit we can compare them using Beat
+            // since it wouldn't matter who led.
+            if card.AdjSuit(top.Suit) == minCard.AdjSuit(top.Suit) {
+                if euchre.Beat(minCard, card, top.Suit) {
+                    minCard = card
+                    minIndex = i
+                }
+            } else {
+            // If the cards are not of the same suit then if the current min is
+            // trump, the new card must be less. Otherwise, as long as the
+            // card in question is not trump and it's value is less then the
+            // current min card, update the trackers.
+                if minCard.AdjSuit(top.Suit) == top.Suit || (card.AdjSuit(top.Suit) != top.Suit && int(card.Value) < int(minCard.Value)) {
+                    minCard = card
+                    minIndex = i
+                }
+            }
+        }
+    }
+
+    hand[minIndex] = top
+    return hand, minCard
 }
 
 func (p *SmartPlayer) Call(hand [5]deck.Card, top deck.Card) (deck.Suit, bool) {

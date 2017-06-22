7d391ed1a88a00f9dd07fb1c9aba3a71e3240c34
Matias Grioni <matgrioni@gmail.com>
2017-06-21 22:32:21 -0700

    Fix pq implentation to remove duplicate methods and proper state assertions.

--- src/ai/pq.go
+++ src/ai/pq.go
@@ -6,19 +6,20 @@ import (
 
 // This file provides a MaxHeap / PriorityQueue for a PQItem. To use the
 // PriorityQueue, create a struct that implements PQItem.
+// TODO: Include example of initialization and usage code.
 
 type PQItem interface {
-    Value() interface{}
+    GetValue() interface{}
     Value(v interface{})
 
-    Priority() int
-    Priority(priority int)
+    GetPriority() float64
+    Priority(priority float64)
 
-    Index() int
+    GetIndex() int
     Index(i int)
 }
 
-type PriorityQueue []*PQItem
+type PriorityQueue []PQItem
 
 func (pq PriorityQueue) Len() int {
     return len(pq)
@@ -27,7 +28,7 @@ func (pq PriorityQueue) Len() int {
 func (pq PriorityQueue) Less(i, j int) bool {
     // Since this is a MaxHeap, and container/heap implements a MinHeap, we need
     // to use greater than.
-    return pq[i].Priority() > pq[j].Priority()
+    return pq[i].GetPriority() > pq[j].GetPriority()
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
@@ -36,29 +37,30 @@ func (pq PriorityQueue) Swap(i, j int) {
     pq[j].Index(j)
 }
 
-func (pq *PriorityQueue) Push(item PQItem) {
+func (pq *PriorityQueue) Push(item interface{}) {
     n := len(*pq)
-    item.Index(n)
-    *pq = append(*pq, item)
+    x := item.(PQItem)
+    x.Index(n)
+    *pq = append(*pq, x)
 }
 
 func (pq *PriorityQueue) Poll() PQItem {
-    return pq[0]
+    return (*pq)[0]
 }
 
-func (pq *PriorityQueue) Pop() PQItem {
+func (pq *PriorityQueue) Pop() interface{} {
     old := *pq
     n := len(old)
 
     item := old[n - 1]
-    item.index = -1
+    item.Index(-1)
     *pq = old[:n - 1]
 
     return item
 }
 
-func (pq *PriorityQueue) Update(item *PQItem, value interface{}, priority int) {
+func (pq *PriorityQueue) Update(item PQItem, value interface{}, priority float64) {
     item.Value(value)
     item.Priority(priority)
-    heap.Fix(pq, item.Index())
+    heap.Fix(pq, item.GetIndex())
 }

69c2b5643a7f86efb9c603f2af7a6c04c043c5ce
Matias Grioni <matgrioni@gmail.com>
2017-08-07 00:08:13 -0700

    Create determinization for states, change and adding helper methods, and testing updates.

--- src/euchre/euchre.go
+++ src/euchre/euchre.go
@@ -3,7 +3,11 @@ package euchre
 import (
     "ai"
     "deck"
+    "math/rand"
+    "time"
 )
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // Contains all the relevant information the setup portion of a euchre game.
 // This includes who was dealer, who called it up, what the top card was, if it
@@ -33,22 +37,150 @@ type Trick struct {
 type State struct {
     Setup Setup
     Player int
-    Hand []deck.Card
+    Hands [][]deck.Card
+    Kitty []deck.Card
     Played []deck.Card
     Prior []Trick
     Move deck.Card
 }
 
+
 func (s State) Hash() interface{} {
     return s.Move
 }
 
-func NewState(setup Setup, player int, hand, played []deck.Card,
-              prior []Trick, move deck.Card) State {
+/*
+ * Create a determinized euchre state off of an incomplete (non-terminal) euchre
+ * state. Information can range from the first move to the last move, and the
+ * incomplete information will be filled in through a determinization process.
+ */
+func (s State) Determinize() {
+    cardsSet := deck.NewCardsSet()
+
+    // Remove all prior cards from contention.
+    for _, trick := range s.Prior {
+        for _, card := range trick.Cards {
+            cardsSet[card] = false
+        }
+    }
+
+    // Remove all played cards from contention.
+    for _, card := range s.Played {
+        cardsSet[card] = false
+    }
+
+    // Remove the top card from contention if it was flipped over, or remove
+    // the discarded card if you were the one who put it down.
+    if s.Setup.Dealer == 0 && s.Setup.PickedUp {
+        cardsSet[s.Setup.Discard] = false
+    } else if !s.Setup.PickedUp {
+        cardsSet[s.Setup.Top] = false
+    }
+
+    idxs := r.Perm(len(cardsSet))
+    cards := extractAvailableCards(cardsSet)
+
+    noSuits := noSuits(s.Prior, s.Setup.Trump)
+    // Go through each opponent player.
+    for player := 1; player < 4; player++ {
+
+        // Go through each card in the random permutation. Go backwards so that
+        // the idxs can be deleted as cards are chosen as valid.
+        for i := len(idxs) - 1; i >= 0; i-- {
+            cardIndex := idxs[i]
+            curCard := cards[cardIndex]
+
+            // If the current card is possible for the current player, then add
+            // it to the current player's hand.
+            possible := true
+            for _, suit := range noSuits[player] {
+                if curCard.AdjSuit(s.Setup.Trump) == suit {
+                    possible = false
+                }
+            }
+
+            // If the current card is possible by the rules of following suit,
+            // then add it. If we have all the cards for this player, then move
+            // on to the next as well. Also remove, the card index so that it
+            // isn't included in further players.
+            if possible {
+                s.Hands[player] = append(s.Hands[player], curCard)
+                idxs = append(idxs[:i], idxs[:i+1]...)
+                if len(s.Hands[player]) == 5 {
+                    break
+                }
+            }
+        }
+    }
+}
+
+
+/*
+ * Create a new state that only has the known information for any given instance
+ * and is therefore undeterminized.
+ *
+ * Args:
+ *  setup: The setup for the game. Information such as top card, dealer, etc.
+ *  player: The current player number.
+ *  hand: The current cards in your hand.
+ *  played: The cards played in the current trick.
+ *  prior: The prior tricks.
+ *  move: The prior card played to get to this state.
+ *
+ * Returns:
+ *  A state that has undeterminized information. This means that not all fields
+ *  of the state have valid values.
+ */
+func NewUndeterminizedState(setup Setup, player int, hand, played []deck.Card,
+                            prior []Trick, move deck.Card) State {
+    // Create blank values for the kitty and other hands other than your own.
+    kitty := make([]deck.Card, 0)
+    hands := make([][]deck.Card, 4)
+    hands[0] = hand
+    hands[1] = make([]deck.Card, 0)
+    hands[2] = make([]deck.Card, 0)
+    hands[3] = make([]deck.Card, 0)
+
     return State {
         setup,
         player,
-        hand,
+        hands,
+        kitty,
+        played,
+        prior,
+        move,
+    }
+}
+
+
+/*
+ * Create a new determinized state. This state may have missing information,
+ * such as in the middle of euchre game. This is fine as long as the structure
+ * of the incomplete information is consistent with actual game play. For
+ * example, players can not have two different card counts, except when it is
+ * the middle of a turn where they differ by one card.
+ *
+ * Args:
+ *  setup: The setup for the game. Information such as top card, dealer, etc.
+ *  player: The current player number.
+ *  hands: A slice of each players cards.
+ *  kitty: The slice of cards in the kitty. TODO: This might not be needed.
+ *  played: The slice of cards played in the current trick.
+ *  prior: The prior tricks.
+ *  move: The move that was played prior to get the current state.
+ *
+ * Returns:
+ *  A new state that is determinized, ie has created some known universe where
+ *  information that is usually not known by an opponent is now assumed.
+ */
+func NewDeterminizedState(setup Setup, player int, hands [][]deck.Card, kitty,
+                          played []deck.Card, prior []Trick,
+                          move deck.Card) State {
+    return State {
+        setup,
+        player,
+        hands,
+        kitty,
         played,
         prior,
         move,
@@ -149,7 +281,9 @@ func Winner(played []deck.Card, trump deck.Suit, led int) int {
     return highPlayer
 }
 
+
 type Engine struct { }
+
 
 func (engine Engine) Favorable(state ai.State, eval int) bool {
     cState := state.(State)
@@ -157,116 +291,53 @@ func (engine Engine) Favorable(state ai.State, eval int) bool {
            (cState.Player % 2 == 1 && eval < 0)
 }
 
+
 func (engine Engine) IsTerminal(state ai.State) bool {
     cState := state.(State)
     return len(cState.Played) == 0 && len(cState.Prior) == 5
 }
 
+
 func (engine Engine) NextStates(state ai.State) []ai.State {
     cState := state.(State)
-    nextStates := make([]ai.State, 0, len(deck.CARDS))
-    var pCards []deck.Card
+    nextStates := make([]ai.State, 0)
 
-    if cState.Player == 0 {
-        pIdxs := Possible(cState.Hand, cState.Played, cState.Setup.Trump)
-        pCards = make([]deck.Card, len(pIdxs))
+    curHand := cState.Hands[cState.Player]
 
-        for i, idx := range pIdxs {
-            pCards[i] = cState.Hand[idx]
-        }
-    } else {
-        all := deck.NewCardsSet()
-        pCards = make([]deck.Card, 0, len(all))
-        noSuits := noSuits(cState.Prior, cState.Player, cState.Setup.Trump)
+    var nPlayed []deck.Card
+    var nPrior []Trick
+    var nPlayer int
+    nmPlayer := (cState.Player + 1) % 4
 
-        for i := 0; i < len(cState.Hand); i++ {
-            all[cState.Hand[i]] = false
-        }
-
-        if cState.Setup.Dealer != cState.Player || !cState.Setup.PickedUp {
-            all[cState.Setup.Top] = false
-        }
-
-        if cState.Setup.Dealer == 0 && cState.Setup.PickedUp {
-            all[cState.Setup.Discard] = false
-        }
-
-        for i := 0; i < len(cState.Prior); i++ {
-            trick := cState.Prior[i]
-            for j := 0; j < len(trick.Cards); j++ {
-                all[trick.Cards[j]] = false
-            }
-        }
-
-        for i := 0; i < len(cState.Played); i++ {
-            all[cState.Played[i]] = false
-        }
-
-        for i := 0; i < len(noSuits); i++ {
-            for card, _ := range all {
-                if card.AdjSuit(cState.Setup.Trump) == noSuits[i] {
-                    all[card] = false
-                    break
-                }
-            }
-        }
-
-        oCards := make([]deck.Card, 0)
-        for card, present := range all {
-            if present {
-                pCards = append(pCards, card)
-            }
-
-            if len(cState.Played) > 0 {
-                if card.AdjSuit(cState.Setup.Trump) ==
-                   cState.Played[0].AdjSuit(cState.Setup.Trump) {
-                    oCards = append(oCards, card)
-                }
-            }
-        }
-
-        if len(oCards) > 0 {
-            pCards = oCards
-        }
-    }
-
-    for i := 0; i < len(pCards); i++ {
-        nCard := pCards[i]
-
-        var nHand []deck.Card
-        if cState.Player == 0 {
-            nHand = make([]deck.Card, 0, len(cState.Hand))
-            for j := 0; j < len(cState.Hand); j++ {
-                if i != j {
-                    nHand = append(nHand, cState.Hand[j])
-                }
-            }
-        } else {
-            nHand = cState.Hand
-        }
-
-        var nPrior []Trick
-        var nPlayed []deck.Card
-        var nPlayer int
-        nmPlayer := (cState.Player + 1) % 4
-
+    for i, card := range curHand {
         if len(cState.Played) < 3 {
+            // Copy the old played cards into memory and add the new card.
             nPlayed = make([]deck.Card, len(cState.Played))
             copy(nPlayed, cState.Played)
+            nPlayed = append(nPlayed, card)
+
+            // The prior tricks stay the same (no new tricks) and the next
+            // player is just the next modulo player.
             nPrior = cState.Prior
-            nPlayed = append(nPlayed, nCard)
             nPlayer = nmPlayer
         } else if len(cState.Played) == 3 {
+            // If this next card ends the trick then copy the old tricks over
+            // and make a new trick out of the current cards.
             nPrior = make([]Trick, len(cState.Prior))
             copy(nPrior, cState.Prior)
 
             var arrPlayed [4]deck.Card
             copy(arrPlayed[:], cState.Played)
-            arrPlayed[3] = nCard
+            arrPlayed[3] = card
 
             nPlayed = make([]deck.Card, 0, 4)
             nPlayer = Winner(arrPlayed[:], cState.Setup.Trump, nmPlayer)
 
+            // If this is the last trick to be played, then don't move the
+            // player to the next one. This is because when we evaluate the
+            // final state, we want it to be in reference to this last player,
+            // not the next one. TODO: Actually I don't really know if this
+            // makes a difference anymore.
             if len(cState.Prior) == 4 {
                 nPlayer = cState.Player
             }
@@ -279,14 +350,20 @@ func (engine Engine) NextStates(state ai.State) []ai.State {
             nPrior = append(nPrior, nextPrior)
         }
 
-        nextState := NewState(cState.Setup, nPlayer, nHand, nPlayed,
-                              nPrior, nCard)
+        nHand := make([]deck.Card, len(curHand))
+        nHand = append(nHand[:i], nHand[i+1:]...)
+        nHands := cState.Hands
+        nHands[cState.Player] = nHand
+
+        nextState := NewDeterminizedState(cState.Setup, nPlayer, nHands,
+                                          cState.Kitty, nPlayed, nPrior, card)
 
         nextStates = append(nextStates, nextState)
     }
 
     return nextStates
 }
+
 
 func (engine Engine) Evaluation(state ai.State) int {
     winCounts0 := 0
@@ -318,14 +395,19 @@ func (engine Engine) Evaluation(state ai.State) int {
 }
 
 
-// A private helper method that returns what suits a given player cannot have.
-// prior  - The list of prior tricks.
-// player - The number of the player. This can be any player, although 0 doesn't
-//          make much sense, since we already know what suits we have!
-// trump  - The current trump suit.
-// Returns a list of the suits that a player cannot have.
-func noSuits(prior []Trick, player int, trump deck.Suit) []deck.Suit {
-    noSuits := make([]deck.Suit, 0, 4)
+/*
+ * A private helper method that returns what suits a given player cannot have.
+ *
+ * Args:
+ *  prior: The list of prior tricks.
+ *  trump: The current trump suit.
+ *
+ * Returns:
+ *  A list of the suits that a player cannot have indexed by player numbers
+ *  in a map.
+ */
+func noSuits(prior []Trick, trump deck.Suit) map[int][]deck.Suit {
+    noSuits := make(map[int][]deck.Suit)
 
     for i := 0; i < len(prior); i++ {
         // For each trick, find out if a user did not follow suit and therefore
@@ -333,13 +415,38 @@ func noSuits(prior []Trick, player int, trump deck.Suit) []deck.Suit {
         trick := prior[i]
         first := trick.Cards[0]
 
-        // Add 4 to player, so that it is guaranteed to be after trick.Led, but
-        // it does not change the final result mod 4.
-        playedCard := trick.Cards[(player + 4 - trick.Led) % 4]
-        if first.AdjSuit(trump) != playedCard.AdjSuit(trump) {
-            noSuits = append(noSuits, first.AdjSuit(trump))
+        for player := 0; player < 4; player++ {
+            // Add 4 to player, so that it is guaranteed to be after trick.Led,
+            // but it does not change the final result mod 4.
+            playedCard := trick.Cards[(player + 4 - trick.Led) % 4]
+            if first.AdjSuit(trump) != playedCard.AdjSuit(trump) {
+                noSuits[player] = append(noSuits[player], first.AdjSuit(trump))
+            }
         }
     }
 
     return noSuits
+}
+
+
+/*
+ * Extracts all cards that exist in the given set and have a value of true, into
+ * a list of cards.
+ *
+ * Args:
+ *  cardsSet: A set of cards. A card exists if it is in the set and its value is
+ *            true.
+ *
+ * Returns:
+ *  A slice of the existing cards in the given set.
+ */
+func extractAvailableCards(cardsSet map[deck.Card]bool) []deck.Card {
+    cards := make([]deck.Card, 0, len(cardsSet))
+    for card, exists := range cardsSet {
+        if exists {
+            cards = append(cards, card)
+        }
+    }
+
+    return cards
 }

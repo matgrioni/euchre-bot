6a15e4f728a030c0097ae55f877a41026db386d8
Matias Grioni <matgrioni@gmail.com>
2017-08-21 16:34:44 -0400

    Added configuration to probabilities for pickup and discard for RandomPlayer.

--- src/player/rand.go
+++ src/player/rand.go
@@ -10,21 +10,48 @@ import (
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 
+/*
+ * A RandomPlayer. This player is non determinisitic, so for the same input, you
+ * can get different outputs. The distribution of picking up, or discarding can
+ * are configurable. Discarding or playing is not since they would naturally
+ * have to depend on card order, which does not necessairily have any meaning.
+ */
 type RandPlayer struct {
+    pickupProb float64
+    callProb float64
 }
 
 
 /*
  * Used to create a new RandPlayer struct that is properly constructed. Returns
  * a RandPlayer pointer.
+ *
+ * Args:
+ *  pickupProb: The probability that the player says the top card should be
+ *              picked up.
+ *  callProb: The probability that the player will call the suit after everybody
+ *            skips the first round.
  */
-func NewRand() (*RandPlayer) {
+func NewRand(pickupProb float64, callProb float64) (*RandPlayer) {
     return &RandPlayer{ }
 }
 
 
+/*
+ * Player decides to pickup with probability pickupProb.
+ *
+ * Args:
+ *  hand: The current hand of the player.
+ *  top: The card on top of the kitty.
+ *  who: The player number designation for the dealer.
+ *
+ * Returns:
+ *  True if the player should tell the dealer to pickup the card. False if pass.
+ *  The player decides the dealer should pickup it up with probability
+ *  pickupProb.
+ */
 func (p *RandPlayer) Pickup(hand []deck.Card, top deck.Card, who int) bool {
-    return r.Intn(2) == 1
+    return r.Float64() < p.pickupProb
 }
 
 
@@ -41,6 +68,20 @@ func (p *RandPlayer) Discard(hand []deck.Card, top deck.Card) ([]deck.Card, deck
 }
 
 
+/*
+ * Player decision method to call suit once all other players have passed on
+ * telling the dealer to pickup.
+ *
+ * Args:
+ *  hand: The hand of the player.
+ *  top: The card on the top of the kitty.
+ *  who: The player number designation for the dealer.
+ *
+ * Returns:
+ *  The suit to call and True if the player should call this suit. False if the
+ *  player should not call it. The player decides to call the suit with
+ *  probability callProb.
+ */
 func (p *RandPlayer) Call(hand []deck.Card, top deck.Card,
                           who int) (deck.Suit, bool) {
     s := deck.SUITS[r.Intn(len(deck.SUITS))]
@@ -48,7 +89,7 @@ func (p *RandPlayer) Call(hand []deck.Card, top deck.Card,
         s = deck.SUITS[r.Intn(len(deck.SUITS))]
     }
 
-    return s, r.Intn(2) == 1
+    return s, r.Float64() < p.callProb
 }
 
 

c08533d1fa1367104e1676e5ac898e480da5d001
Matias Grioni <matgrioni@gmail.com>
2017-08-22 16:47:21 -0400

    Moved other code over into new package file.

--- src/euchre/logic.go
+++ src/euchre/logic.go
@@ -96,6 +96,121 @@ func WinnerAlone(played []deck.Card, trump deck.Suit, led int, alone int) int {
 }
 
 
+
+
+/*
+ * Given a player's current hand and the cards that have been played, the
+ * possible cards for a player to play are returned. In other words, all cards
+ * in the player's hand that match the suit of the led card are returned or all
+ * cards otherwise. Also, the actual cards are not returned, rather their
+ * position in the hand is returned. This is to make deletion easier.
+ *
+ * Args:
+ *  hand: The player's current cards.
+ *  played: The cards that have already been played.
+ *  trump: The suit that is currently trump.
+ *
+ * Returns:
+ *  The index of cards in hand that can be played according to euchre rules.
+ */
+func Possible(hand, played []deck.Card, trump deck.Suit) []int {
+    possible := make([]int, 0, len(hand))
+    if len(played) > 0 {
+        for i := range hand {
+            if hand[i].AdjSuit(trump) == played[0].AdjSuit(trump) {
+                possible = append(possible, i)
+            }
+        }
+    }
+
+    if len(possible) == 0 {
+        for i := range hand {
+            possible = append(possible, i)
+        }
+    }
+
+    return possible
+}
+
+
+/*
+ * Creates a new copy of the hands in memory from the given state.
+ *
+ * Args:
+ *  state: A euchre state whose player hands are to be copied.
+ *
+ * Returns:
+ *  A copy of the players hands. A [][]deck.Card copy.
+ */
+func copyAllHands(state State) [][]deck.Card {
+    copyHands := make([][]deck.Card, len(state.Hands))
+    for i, hand := range state.Hands {
+        copyHand := make([]deck.Card, len(hand))
+        copy(copyHand, hand)
+
+        copyHands[i] = copyHand
+    }
+
+    return copyHands
+}
+
+
+/*
+ * Extracts all cards that exist in the given set and have a value of true, into
+ * a list of cards.
+ *
+ * Args:
+ *  cardsSet: A set of cards. A card exists if it is in the set and its value is
+ *            true.
+ *
+ * Returns:
+ *  A slice of the existing cards in the given set.
+ */
+func extractAvailableCards(cardsSet map[deck.Card]bool) []deck.Card {
+    cards := make([]deck.Card, 0, len(cardsSet))
+    for card, exists := range cardsSet {
+        if exists {
+            cards = append(cards, card)
+        }
+    }
+
+    return cards
+}
+
+
+/*
+ * A private helper method that returns what suits a given player cannot have.
+ *
+ * Args:
+ *  prior: The list of prior tricks.
+ *  trump: The current trump suit.
+ *
+ * Returns:
+ *  A list of the suits that a player cannot have indexed by player numbers
+ *  in a map.
+ */
+func noSuits(prior []Trick, trump deck.Suit) map[int][]deck.Suit {
+    noSuits := make(map[int][]deck.Suit)
+
+    for i := 0; i < len(prior); i++ {
+        // For each trick, find out if a user did not follow suit and therefore
+        // does not have this suit.
+        trick := prior[i]
+        first := trick.Cards[0]
+
+        for player := 0; player < 4; player++ {
+            // Add 4 to player, so that it is guaranteed to be after trick.Led,
+            // but it does not change the final result mod 4.
+            playedCard := trick.Cards[(player + 4 - trick.Led) % 4]
+            if first.AdjSuit(trump) != playedCard.AdjSuit(trump) {
+                noSuits[player] = append(noSuits[player], first.AdjSuit(trump))
+            }
+        }
+    }
+
+    return noSuits
+}
+
 /*
  * A function that returns the winning player (using the same number designation
  * as before) based on the trump suit, the cards that have been played, what the
@@ -141,39 +256,4 @@ func winner(played []deck.Card, trump deck.Suit, led int, alone int) int {
     }
 
     return highPlayer
-}
-
-
-/*
- * Given a player's current hand and the cards that have been played, the
- * possible cards for a player to play are returned. In other words, all cards
- * in the player's hand that match the suit of the led card are returned or all
- * cards otherwise. Also, the actual cards are not returned, rather their
- * position in the hand is returned. This is to make deletion easier.
- *
- * Args:
- *  hand: The player's current cards.
- *  played: The cards that have already been played.
- *  trump: The suit that is currently trump.
- *
- * Returns:
- *  The index of cards in hand that can be played according to euchre rules.
- */
-func Possible(hand, played []deck.Card, trump deck.Suit) []int {
-    possible := make([]int, 0, len(hand))
-    if len(played) > 0 {
-        for i := range hand {
-            if hand[i].AdjSuit(trump) == played[0].AdjSuit(trump) {
-                possible = append(possible, i)
-            }
-        }
-    }
-
-    if len(possible) == 0 {
-        for i := range hand {
-            possible = append(possible, i)
-        }
-    }
-
-    return possible
 }

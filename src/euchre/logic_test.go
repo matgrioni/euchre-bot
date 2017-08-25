cb09cf208daf10143e766936ab0875554c552079
Matias Grioni <matgrioni@gmail.com>
2017-08-24 22:14:34 -0400

    Final reorganization of test into euchre logic file.

--- src/euchre/logic_test.go
+++ src/euchre/logic_test.go
@@ -2,20 +2,16 @@ package euchre
 
 import (
     "deck"
+    "fmt"
     "testing"
 )
 
 
-/*
- * A test struct for going alone. This struct constitutes one test case for
- * the WinnerAlone method.
- */
-type winnerTest struct {
-    played []deck.Card
+type beatTest struct {
+    a deck.Card
+    b deck.Card
     trump deck.Suit
-    led int
-    alone int
-    expected int
+    expected bool
 }
 
 
@@ -26,262 +22,81 @@ type noSuitsTest struct {
 }
 
 
-/*
- * Test euchre#Beat. A helper to determine which card wins in a head to head
- * faceoff where one card is chosen to be leading and there is a defined
- * trump suit.
- */
-
-
-func TestBeatTrumpNonTrump(t *testing.T) {
-    trump := deck.H
-    card1 := deck.Card{ deck.H, deck.Ten }
-    card2 := deck.Card{ deck.C, deck.A }
-
-    if !Beat(card1, card2, trump) {
-        t.Errorf("Expected %s to beat %s with trump %s", card1, card2, trump)
-    }
+type possibleTest struct {
+    hand []deck.Card
+    played []deck.Card
+    trump deck.Suit
+    expected []int
 }
 
 
-func TestBeatNonTrumpTrump(t *testing.T) {
-    trump := deck.C
-    card1 := deck.Card{ deck.S, deck.Q }
-    card2 := deck.Card{ deck.C, deck.A }
-
-    if Beat(card1, card2, trump) {
-        t.Errorf("Expected %s to beat %s with trump %s", card2, card1, trump)
-    }
-}
-
-
-func TestBeatDifferentNonTrumpFirstLess(t *testing.T) {
-    trump := deck.D
-    card1 := deck.Card{ deck.S, deck.Nine }
-    card2 := deck.Card{ deck.C, deck.A }
-
-    if !Beat(card1, card2, trump) {
-        t.Errorf("Expected %s to beat %s with trump %s", card1, card2, trump)
-    }
-}
-
-
-func TestBeatDifferentNonTrumpFirstGreater(t *testing.T) {
-    trump := deck.D
-    card1 := deck.Card{ deck.S, deck.A }
-    card2 := deck.Card{ deck.C, deck.Nine }
-
-    if !Beat(card1, card2, trump) {
-        t.Errorf("Expected %s to beat %s with trump %s", card1, card2, trump)
-    }
-}
-
-
-func TestBeatSameSuit(t *testing.T) {
-    trump := deck.D
-    card1 := deck.Card{ deck.S, deck.A }
-    card2 := deck.Card{ deck.S, deck.Nine }
-
-    if !Beat(card1, card2, trump) {
-        t.Errorf("Expected %s to beat %s with trump %s", card1, card2, trump)
-    }
-}
-
-
-func TestBeatBowers(t *testing.T) {
-    trump := deck.D
-    card1 := deck.Card{ deck.H, deck.J }
-    card2 := deck.Card{ deck.D, deck.J }
-
-    if Beat(card1, card2, trump) {
-        t.Errorf("Expected %s to beat %s with trump %s", card2, card1, trump)
-    }
+type winnerTest struct {
+    played []deck.Card
+    trump deck.Suit
+    led int
+    alone int
+    expected int
 }
 
 
 /*
- * Test Winner
+ * Test Beat. A helper to determine which card wins in a head to head faceoff
+ * where one card is chosen to be leading and there is a defined trump suit.
  */
 
-var winnerTests = []winnerTest {
-    /*
-     * The start of the non-going alone tests (i.e. where everybody plays).
-     */
-
-    // Only one trump
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.D, deck.A },
-            deck.Card { deck.H, deck.Q },
-            deck.Card { deck.D, deck.Ten },
-            deck.Card { deck.D, deck.Q },
-        },
+var beatTests = []beatTest {
+    // Trump beats non-trump
+    beatTest {
+        deck.Card { deck.H, deck.Ten },
+        deck.Card { deck.C, deck.A },
         deck.H,
-        2,
-        -1,
-        3,
+        true,
     },
 
-    // Only one trump v2
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.H, deck.A },
-            deck.Card { deck.H, deck.Nine },
-            deck.Card { deck.S, deck.A },
-            deck.Card { deck.D, deck.Q },
-        },
+    // Trump beats non-trump v2, with orders flipped
+    beatTest {
+        deck.Card { deck.S, deck.Q },
+        deck.Card { deck.C, deck.A },
+        deck.C,
+        false,
+    },
+
+    // If suits don't match and no trumps, then leader wins
+    beatTest {
+        deck.Card { deck.S, deck.Nine },
+        deck.Card { deck.C, deck.A },
         deck.D,
-        2,
-        -1,
-        1,
+        true,
     },
 
-    // Only one trump v3
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.D, deck.Q },
-            deck.Card { deck.D, deck.K },
-            deck.Card { deck.C, deck.Ten },
-            deck.Card { deck.D, deck.A },
-        },
-        deck.C,
-        1,
-        -1,
-        3,
+    // The higher card of the same suit wins
+    beatTest {
+        deck.Card { deck.S, deck.A },
+        deck.Card { deck.S, deck.Nine },
+        deck.D,
+        true,
     },
 
-    // Only one trump v4
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.H, deck.A },
-            deck.Card { deck.H, deck.J },
-            deck.Card { deck.H, deck.Q },
-            deck.Card { deck.C, deck.Ten },
-        },
-        deck.C,
-        2,
-        -1,
-        1,
-    },
-
-    // Only one trump v5
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.H, deck.A },
-            deck.Card { deck.H, deck.J },
-            deck.Card { deck.H, deck.Q },
-            deck.Card { deck.C, deck.Ten },
-        },
-        deck.C,
-        2,
-        -1,
-        1,
-    },
-
-    // All non-trump
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.H, deck.Ten },
-            deck.Card { deck.H, deck.A },
-            deck.Card { deck.H, deck.Q },
-            deck.Card { deck.H, deck.Ten },
-        },
-        deck.C,
-        3,
-        -1,
-        0,
-    },
-
-    // Multiple trump in one trick.
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.D, deck.J },
-            deck.Card { deck.C, deck.Ten },
-            deck.Card { deck.S, deck.J },
-            deck.Card { deck.C, deck.A },
-        },
-        deck.C,
-        1,
-        -1,
-        3,
-    },
-
-    /*
-     * The start of going alone tests.
-     */
-
-    /*
-     * A test where the discarded player doesn't matter since the winner is
-     * before him.
-     */
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.H, deck.A },
-            deck.Card { deck.C, deck.Ten },
-            deck.Card { deck.C, deck.Q },
-        },
-        deck.H,
-        0,
-        0,
-        0,
-    },
-
-    /*
-     * A test where the winner is after the person who is cucked.
-     */
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.D, deck.J },
-            deck.Card { deck.H, deck.J },
-            deck.Card { deck.S, deck.Q },
-        },
-        deck.H,
-        1,
-        0,
-        3,
-    },
-
-    /*
-     * A test where the leader is the one going alone.
-     */
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.H, deck.Q },
-            deck.Card { deck.S, deck.A },
-            deck.Card { deck.H, deck.A },
-        },
-        deck.H,
-        1,
-        1,
-        0,
-    },
-
-    /*
-     * Just another normal test case for the winner when going alone.
-     */
-    winnerTest {
-        []deck.Card {
-            deck.Card { deck.C, deck.Nine },
-            deck.Card { deck.H, deck.Q },
-            deck.Card { deck.S, deck.J },
-        },
-        deck.S,
-        2,
-        1,
-        1,
+    // The right bower beats the left bower
+    beatTest {
+        deck.Card { deck.H, deck.J },
+        deck.Card { deck.D, deck.J },
+        deck.D,
+        false,
     },
 }
 
 
 /*
- * Tests all the above tests to make sure the expected winner is actually seen.
+ * Run all the beat tests defined above and output any errors in reference to
+ * the expected and actual values and the index of the test.
  */
-func TestWinnerAlone(t *testing.T) {
-    for _, test := range winnerTests {
-        res := Winner(test.played, test.trump, test.led, test.alone)
+func TestBeat(t *testing.T) {
+    for i, test := range beatTests {
+        res := Beat(test.a, test.b, test.trump)
 
         if res != test.expected {
-            t.Errorf("Expected error to be %d but got %d\n", test.expected, res)
+            errorOut(t, test.expected, res, "beat", i)
         }
     }
 }
@@ -651,12 +466,12 @@ var noSuitsTests = []noSuitsTest {
  * maps contain the same content but not necessairily in the same order.
  */
 func TestNoSuits(t *testing.T) {
-    for _, test := range noSuitsTests {
+    for i, test := range noSuitsTests {
         res := noSuits(test.prior, test.trump)
 
         // Check that the two maps are the same size.
         if len(res) != len(test.expected) {
-            errorOut(t, test.expected, res, "no suits")
+            errorOut(t, test.expected, res, "no suits", i)
         }
 
         // For each player check that the suits are the same.
@@ -664,7 +479,7 @@ func TestNoSuits(t *testing.T) {
             // Check if this player does exists in the actual results.
             actualTmp, ok := res[player]
             if !ok {
-                errorOut(t, test.expected, res, "no suits")
+                errorOut(t, test.expected, res, "no suits", i)
             }
             actual := suitsSliceToSet(actualTmp)
             expected := suitsSliceToSet(suits)
@@ -672,15 +487,265 @@ func TestNoSuits(t *testing.T) {
             // Check that the two sets of suits are the same length, and then
             // have the same contents.
             if len(actual) != len(expected) {
-                errorOut(t, test.expected, res, "no suits")
+                errorOut(t, test.expected, res, "no suits", i)
             }
 
             for suit, _ := range expected {
                 _, ok := actual[suit]
                 if !ok {
-                    errorOut(t, test.expected, res, "no suits")
+                    errorOut(t, test.expected, res, "no suits", i)
                 }
             }
+        }
+    }
+}
+
+
+/*
+ * Test Possible. This returns the cards that can be played given a hand and the
+ * cards currently played. The actual cards are not returned, the indices of the
+ * possible cards are provided.
+ */
+
+var possibleTests = []possibleTest {
+    // Can't follow lead card, so all cards are possible.
+    possibleTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.Ten },
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.C, deck.J },
+            deck.Card { deck.C, deck.K },
+            deck.Card { deck.D, deck.J },
+        },
+        []deck.Card {
+            deck.Card { deck.S, deck.Ten },
+        },
+        deck.H,
+        []int {
+            0, 1, 2, 3, 4,
+        },
+    },
+
+    // Can follow with one card.
+    possibleTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.Ten },
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.C, deck.J },
+            deck.Card { deck.C, deck.K },
+            deck.Card { deck.S, deck.J },
+        },
+        []deck.Card {
+            deck.Card { deck.S, deck.Ten },
+        },
+        deck.H,
+        []int {
+            4,
+        },
+    },
+}
+
+
+func TestPossible(t *testing.T) {
+    for i, test := range possibleTests {
+        res := Possible(test.hand, test.played, test.trump)
+
+        if len(res) != len(test.expected) {
+            errorOut(t, test.expected, res, "possible", i)
+        }
+
+        for j := 0; j < len(test.expected); j++ {
+            if test.expected[j] != res[j] {
+                errorOut(t, test.expected, res, "possible", i)
+            }
+        }
+    }
+}
+
+
+/*
+ * Test Winner
+ */
+
+var winnerTests = []winnerTest {
+    /*
+     * The start of the non-going alone tests (i.e. where everybody plays).
+     */
+
+    // Only one trump
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.D, deck.A },
+            deck.Card { deck.H, deck.Q },
+            deck.Card { deck.D, deck.Ten },
+            deck.Card { deck.D, deck.Q },
+        },
+        deck.H,
+        2,
+        -1,
+        3,
+    },
+
+    // Only one trump v2
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.H, deck.Nine },
+            deck.Card { deck.S, deck.A },
+            deck.Card { deck.D, deck.Q },
+        },
+        deck.D,
+        2,
+        -1,
+        1,
+    },
+
+    // Only one trump v3
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.D, deck.Q },
+            deck.Card { deck.D, deck.K },
+            deck.Card { deck.C, deck.Ten },
+            deck.Card { deck.D, deck.A },
+        },
+        deck.C,
+        1,
+        -1,
+        3,
+    },
+
+    // Only one trump v4
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.H, deck.J },
+            deck.Card { deck.H, deck.Q },
+            deck.Card { deck.C, deck.Ten },
+        },
+        deck.C,
+        2,
+        -1,
+        1,
+    },
+
+    // Only one trump v5
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.H, deck.J },
+            deck.Card { deck.H, deck.Q },
+            deck.Card { deck.C, deck.Ten },
+        },
+        deck.C,
+        2,
+        -1,
+        1,
+    },
+
+    // All non-trump
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.Ten },
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.H, deck.Q },
+            deck.Card { deck.H, deck.Ten },
+        },
+        deck.C,
+        3,
+        -1,
+        0,
+    },
+
+    // Multiple trump in one trick.
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.D, deck.J },
+            deck.Card { deck.C, deck.Ten },
+            deck.Card { deck.S, deck.J },
+            deck.Card { deck.C, deck.A },
+        },
+        deck.C,
+        1,
+        -1,
+        3,
+    },
+
+    /*
+     * The start of going alone tests.
+     */
+
+    /*
+     * A test where the discarded player doesn't matter since the winner is
+     * before him.
+     */
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.A },
+            deck.Card { deck.C, deck.Ten },
+            deck.Card { deck.C, deck.Q },
+        },
+        deck.H,
+        0,
+        0,
+        0,
+    },
+
+    /*
+     * A test where the winner is after the person who is cucked.
+     */
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.D, deck.J },
+            deck.Card { deck.H, deck.J },
+            deck.Card { deck.S, deck.Q },
+        },
+        deck.H,
+        1,
+        0,
+        3,
+    },
+
+    /*
+     * A test where the leader is the one going alone.
+     */
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.H, deck.Q },
+            deck.Card { deck.S, deck.A },
+            deck.Card { deck.H, deck.A },
+        },
+        deck.H,
+        1,
+        1,
+        0,
+    },
+
+    /*
+     * Just another normal test case for the winner when going alone.
+     */
+    winnerTest {
+        []deck.Card {
+            deck.Card { deck.C, deck.Nine },
+            deck.Card { deck.H, deck.Q },
+            deck.Card { deck.S, deck.J },
+        },
+        deck.S,
+        2,
+        1,
+        1,
+    },
+}
+
+
+/*
+ * Run all the above tests to make sure the expected winner is actually seen.
+ */
+func TestWinner(t *testing.T) {
+    for _, test := range winnerTests {
+        res := Winner(test.played, test.trump, test.led, test.alone)
+
+        if res != test.expected {
+            t.Errorf("Expected error to be %d but got %d\n", test.expected, res)
         }
     }
 }
@@ -716,8 +781,10 @@ func suitsSliceToSet(suits []deck.Suit) map[deck.Suit]bool {
  *  expected: The expected value from the test case.
  *  actual: The value actually calculated.
  *  test: The test name to output for clarification purposes.
+ *  index: The index of the test so that the exact test is known.
  */
 func errorOut(t *testing.T, expected interface{}, actual interface{},
-              test string) {
-    t.Errorf("Expected %v but got %v for %s\n", expected, actual, test)
+              test string, index int) {
+    testName := fmt.Sprintf("%s[%d]", test, index)
+    t.Errorf("Expected %v but got %v for %s\n", expected, actual, testName)
 }

65b3dd9768da070c14219e65861fd4f565ec7943
Matias Grioni <matgrioni@gmail.com>
2017-09-08 11:48:39 -0400

    Remove Minimax specific tree search types.

--- src/ai/minimax.go
+++ src/ai/minimax.go
@@ -3,28 +3,6 @@ package ai
 import "math"
 
 
-type MinimaxState interface { }
-
-
-/*
- * A type to represent a move in a game. A move consists of an action, and a
- * successsor state. In the case where the game is in a terminal state, this is
- * represented via a nil action.
- */
-type Move struct {
-    Action interface{}
-    State MinimaxState
-}
-
-
-type MinimaxEngine interface {
-    Favorable(state MinimaxState) bool
-    IsTerminal(state MinimaxState) bool
-    Evaluation(state MinimaxState) float64
-    Successors(state MinimaxState) []Move
-}
-
-
 /*
  * Uses minimax adversarial tree search to find the optimal move in a game.
  *
@@ -37,7 +15,7 @@ type MinimaxEngine interface {
  *  with it. This move struct provides the action needed to get to this state
  *  and the state it will send you to.
  */
-func Minimax(state MinimaxState, engine MinimaxEngine) (float64, Move) {
+func Minimax(state TSState, engine TSEngine) (float64, Move) {
     return minimaxHelper(state, engine, math.Inf(-1), math.Inf(1))
 }
 
@@ -57,7 +35,7 @@ func Minimax(state MinimaxState, engine MinimaxEngine) (float64, Move) {
  *  with it. This move struct provides the action needed to get to this state
  *  and the state it will send you to.
  */
-func minimaxHelper(state MinimaxState, engine MinimaxEngine, alpha float64,
+func minimaxHelper(state TSState, engine TSEngine, alpha float64,
                    beta float64) (float64, Move) {
     if engine.IsTerminal(state) {
         return engine.Evaluation(state), Move { nil, state }
